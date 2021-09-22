82ce87b5ed32204a12faac2b97a0bd05695a5019
anhld <wolfwithcode@gmail.com>
2021-09-22 15:12:25 +0700

    back up

---
+++ support_resistance.py
@@ -0,0 +1,24 @@
+# import necessary libraries
+import matplotlib.pyplot as plt
+import matplotlib.dates as mpl_dates
+from mplfinance.original_flavor import candlestick_ohlc
+import math
+import numpy as np
+import yfinance as yf
+import pandas as pd
+# !pip install yfinance
+# !pip install mplfinance
+# get stock prices using yfinance library
+
+
+def get_stock_price(symbol):
+    df = yf.download(symbol, start='2021-02-01', threads=False)
+    df['Date'] = pd.to_datetime(df.index)
+    df['Date'] = df['Date'].apply(mpl_dates.date2num)
+    df = df.loc[:, ['Date', 'Open', 'High', 'Low', 'Close']]
+    return df
+
+
+symbol = 'COST'
+df = get_stock_price(symbol)
+df.to_csv('stock.csv')
